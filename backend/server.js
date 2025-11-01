dc22cb303d75d794aba3037e677e58798c247931
Abhii-22 <137554703+Abhii-22@users.noreply.github.com>
2025-11-01 15:27:34 +0530

    new commits

--- backend/server.js
+++ backend/server.js
@@ -15,15 +15,7 @@ app.use(express.json({ limit: '200mb' }));
 app.use(express.urlencoded({ limit: '200mb', extended: true }));
 
 // Serve static files
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
-  });
-} else {
-  app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-}
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Database Connection
 mongoose.connect(process.env.MONGO_URI, {
